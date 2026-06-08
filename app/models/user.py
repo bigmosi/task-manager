99141a9bc4583114bc04672cb95f4a79fc9159a6
bigmosirtv <amos.kinyera@raisingthevillage.org>
2026-06-08 19:43:18 +0300

    implemented users routes

--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, Boolean, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Boolean, Integer, String
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, nullable=False)
-    email = Column(String,unique=True, nullable=False)
-    password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
 
     tasks = relationship("Task", back_populates="owner",cascade="all, delete")
